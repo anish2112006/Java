25968ccf10f32c0c20af5fc41005c92ad009b4c8
anish2112006 <anishanish2112006@gmail.com>
2025-04-10 12:15:57 +0530

    Add files via upload

---
+++ unaryOperator.java
@@ -0,0 +1,10 @@
+public class unaryOperator {
+   public static void main(String args[]){
+int d=5;
+System.out.println("Original d="+d);
+System.out.println("Post-increment++d="+(d++));
+System.out.println("After d++=>"+d);
+System.out.println("Pre-increment++d+"+(++d));
+System.out.println("pre-decrement--d="+(--d));
+}
+}
