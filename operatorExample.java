548865dabd114911206c2cd611861f973f83f438
anish2112006 <anishanish2112006@gmail.com>
2025-04-10 12:02:08 +0530

    Add files via upload

---
+++ operatorExample.java
@@ -0,0 +1,11 @@
+public class operatorExample {
+   public static void main(String args[]){
+int a=10;
+int c=a;
+System.out.println("Initial c="+c);
+c+=5;
+System.out.println("After c+=5:"+c);
+c*=2;
+System.out.println("After c*=2:"+c);
+}
+}
