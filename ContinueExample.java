c23c145e47c206836316b8afd414ff98c572e580
anish2112006 <anishanish2112006@gmail.com>
2025-04-10 13:19:48 +0530

    Add files via upload

---
+++ ContinueExample.java
@@ -0,0 +1,14 @@
+public class ContinueExample {
+    public static void main(String[] args) {
+        System.out.println("Printing odd numbers between 1 and 10:");
+
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+        System.out.println("Done.");
+    }
+}
